f3da21db075c8fd51d0578ac81a0c3dd96756ddd
Connor Hollis <connorhollis3d@gmail.com>
2022-02-27 11:08:14 -0700

    updated get single user route to display no user found instead of null if no user found

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -25,11 +25,14 @@ const userController = {
                 select: '-__v'
             })
             .select('-__v')
-            .then(dbUserData => res.json(dbUserData))
-            .catch(err => {
-                console.log(err);
-                res.sendStatus(400);
-            });
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
     },
 
     // create user
